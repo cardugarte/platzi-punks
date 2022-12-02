f2520695bd5a5d542accbf467cc52fbf8a67bb0e
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-12-02 18:54:09 -0300

    Se agregan funcionalidades al contrato

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract PlatziPunks is ERC721, ERC721Enumerable {
   uint256 public maxSupply;
@@ -20,6 +21,29 @@ contract PlatziPunks is ERC721, ERC721Enumerable {
     _safeMint(msg.sender, current);
   }
 
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override
+    returns(string memory) {
+      require(_exists(tokenId), "ERC721 Metadata: URI query for nonexistent token");
+      bytes memory jsonURI = abi.encodePacked(
+            '"name" : "Platzi Punks"', 
+            tokenId,
+            '", "description" : "Randomized Avatars", "image" : ", '
+            "// TODO : calculate images URL",
+            '"}'
+        ); 
+
+        return string (
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(jsonURI)
+            )   
+        );
+  }
+
+  //override required
   function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         override(ERC721, ERC721Enumerable)
