5cc51e6a57b05abcfaed94bf67e86834572ca6e3
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-12-12 11:29:23 -0300

    Se corrige el error en los tests

--- test/PlatziPunks.js
+++ test/PlatziPunks.js
@@ -1,22 +1,25 @@
 const { expect } = require('chai')
+// const { ethers } = require('ethers')
 const { ethers } = require('hardhat')
 
 describe('Platzi Punks Contract', () => {
-  const setup = async ({ maxSupply = 10000}) => {
-    const [ owner ] = await ethers.getSigners()
-    const PlatziPunks = await ethers.getContractFactory('PlatziPunks')
-    const deployed = PlatziPunks.deploy(maxSupply)
+  const setup = async ({ maxSupply = 10000 }) => {
+    const [owner] = await ethers.getSigners()
+    const PlatziPunks = await ethers.getContractFactory("PlatziPunks")
+    const deployed = await PlatziPunks.deploy(maxSupply)
     return {
       owner,
-      deployed,
+      deployed
     }
   }
-  describe('Deployment', () => {
-    it('Sets max supply to passed param', async () => {
-      const maxSupply = 4000
-      const { deployed } = await setup({ maxSupply })
-      const returnedMaxSupply = await deployed.maxSupply()
-      expect(maxSupply).to.equal(returnedMaxSupply)
+  describe('Platzi Punks Contract', () => {
+    describe('Deployment', () => {
+      it('sets max supply to passed param', async () => {
+        const maxSupply = 4000
+        const { deployed } = await setup({ maxSupply })
+        const returnedMaxSupply = await  deployed.maxSupply()
+        expect(maxSupply).to.equal(returnedMaxSupply)
+      })
     })
   })
 })
